267bb98fe857749f789bc328764db27bc577b1e5
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-01 08:38:40 -0300

    Clonando repositorio

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -2,7 +2,7 @@ export default function Cabecalho() {
   return (
     <>
       <header>
-        <h1>Vite + React / Coded By - PF0670</h1>
+        <h1>Vite + React / Coded By - RM552422</h1>
         <ul>
           <li>
             <a href="#">Item - 1</a>
