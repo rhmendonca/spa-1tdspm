14fbec63b582f84fe926a32332aac8709c61f528
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-04 12:04:53 -0300

    Armazenando o ObjetoProduto em um useState.

--- src/routes/EditarProdutos.jsx
+++ src/routes/EditarProdutos.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { ListaProdutos } from "../components/ListaProdutos";
+import { useState } from "react";
 
 export default function EditarProdutos() {
 
@@ -9,14 +10,39 @@ export default function EditarProdutos() {
     const {id} = useParams();
 
     const produtoRecuperadoDaListaById = ListaProdutos.filter(item => item.id == id );
-
+    
+    const [produto, setProduto] = useState({
+      id: produtoRecuperadoDaListaById[0].id,
+      nome: produtoRecuperadoDaListaById[0].nome,
+      desc: produtoRecuperadoDaListaById[0].desc,
+      valor: produtoRecuperadoDaListaById[0].valor
+    });
 
   return (
     <div>
         <h1>EDITAR - PRODUTOS</h1>
 
         <div>
-          
+          <form>
+            <fieldset>
+              <legend>PRODUTO SELECIONADO</legend>
+              <div>
+                <label htmlFor="idNome">Nome do Produto:</label>
+                <input type="text" name="nome" id="idNome" defaultValue={produto.nome}/>
+              </div>
+              <div>
+                <label htmlFor="idDesc">Descrição do Produto:</label>
+                <input type="text" name="desc" id="idDesc" defaultValue={produto.desc}/>
+              </div>
+              <div>
+                <label htmlFor="idValor">Valor do Produto:</label>
+                <input type="text" name="valor" id="idValor" defaultValue={produto.valor}/>
+              </div>
+              <div>
+                <button>EDITAR</button>
+              </div>
+            </fieldset>
+          </form>
         </div>
 
     </div>
