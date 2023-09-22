a3ffb429481294279022f15c2da556f84c24f2ea
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-22 09:02:45 -0300

    Refatoramento do componente Produtos.

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -3,23 +3,16 @@ import { ListaProdutos } from "../components/ListaProdutos";
 import {GrFormEdit as Editar} from "react-icons/gr";
 import {RiDeleteBin2Fill as Excluir} from "react-icons/ri";
 import style from "./Produtos.module.css";
-import { useEffect, useState } from "react";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  const [counter, setCounter] = useState(0);
 
-  useEffect(()=>{
-    console.log("Este useEffect renderiza sempre!");
-  });
+
 
   return ( 
     <div>
       <h1>LISTA DE PRODUTOS</h1>
-      <div>
-        <button onClick={()=> setCounter(1)}>COUNTER - {counter}</button>
-      </div>
       <table className={style.tblEstilo}>
         <thead>
         <tr>
@@ -52,3 +45,30 @@ export default function Produtos() {
     </div>
   )
 }
+
+//Exemplo de useEfect
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza sempre! " + counter);
+// });
+
+// const [counter, setCounter] = useState(0);
+
+// const [listaProdutosLocal, setListaProdutosLocal] = useState([{}]);
+
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza apenas uma vez!");
+//   setListaProdutosLocal(ListaProdutos);
+// },[ ]);
+
+// const [counter2, setCounter2] = useState(0)
+
+// useEffect(()=>{
+//   console.log("Este useEffect renderiza sempre que o objeto/variável ou elemento que está sendo monitorado no array de dependências sofra alguma atualização.");
+// },[counter2]);
+
+// <div>
+// <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+// </div>
+// <div>
+// <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+// </div>
