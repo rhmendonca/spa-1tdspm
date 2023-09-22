cbcd4e735cf52f585ab142d914c4bf5175d6a0d6
rhmendonca <rafaelhenriquedemendonca@outlook.com>
2023-09-22 09:47:20 -0300

    Finalizando.

--- src/routes/InserirProduto.jsx
+++ src/routes/InserirProduto.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { ListaProdutos } from "../components/ListaProdutos";
+import { useNavigate } from "react-router-dom";
 
 export default function InserirProduto() {
     document.title = "CADASTRAR";
+
+    const navigate = useNavigate();
 
     //Gerando um novo ID
     let novoId = (ListaProdutos[ListaProdutos.length - 1].id + 1);
@@ -14,20 +17,40 @@ export default function InserirProduto() {
         valor:""
     });
 
+    const handleChange = (e)=>{
+        e.preventDefault();
+        //Destructuring
+        const {name,value} = e.target;
+        //Através da função set do useState, vamos adicionar o valor(value), na propriedade name que é a mesma que foi declarada no useState produto.
+        //Utilize o SPREAD, para tornar a função mais simples!!
+        setProduto({...produto,[name]:value});
+        // if(name == "nome"){
+        //     setProduto({"nome":value});
+        // }else if(name == "desc"){
+        //     setProduto({"desc":value});
+        // }else if(name == "valor"){
+        //     setProduto({"valor":value});
+        // }
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        ListaProdutos.push(produto);
+        navigate("/produtos");
+    }
+
+
   return (
     <div>
         <h1>Cadastro de Produtos</h1>
         
         <div>
-          <form onSubmit={}>
+          <form onSubmit={handleSubmit}>
             <fieldset>
-              <legend>PRODUTO SELECIONADO</legend>
-              <div>
-                <input type="hidden" name="id" value={produto.id} onChange={handleChange}/>
-              </div>
+              <legend>PRODUTO</legend>
               <div>
                 <label htmlFor="idNome">Nome do Produto:</label>
-                <input type="text" name="nome" id="idNome" value={produto.nome} onChange={handleChange}/>
+                <input type="text" name="nome" id="idNome" value={produto.nome}onChange={handleChange} />
               </div>
               <div>
                 <label htmlFor="idDesc">Descrição do Produto:</label>
@@ -38,7 +61,7 @@ export default function InserirProduto() {
                 <input type="text" name="valor" id="idValor" value={produto.valor} onChange={handleChange}/>
               </div>
               <div>
-                <button>EDITAR</button>
+                <button>CADASTRAR</button>
               </div>
             </fieldset>
           </form>
